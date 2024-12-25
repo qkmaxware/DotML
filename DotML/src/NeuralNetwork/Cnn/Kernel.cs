0702c3f9b410e7b85d79c22ab4fadf4c4af1b1fa
Colin Halseth <qkmaxware@gmail.com>
2024-12-25 09:56:46 -0800

    Moved layer types to their own folder

--- DotML/src/NeuralNetwork/Cnn/Kernel.cs
+++ DotML/src/NeuralNetwork/Cnn/Kernel.cs
@@ -56,10 +56,46 @@ public static class Kernels {
     }
 
     /// <summary>
+    /// Identity kernel
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> IdentityKernel(int size) {
+        return Matrix<double>.Identity(size);
+    }
+
+    /// <summary>
+    /// Averages the pixel values in the kernel area, resulting in a blur effect
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> BoxBlurKernel(int size) {
+        const double oneNinth = 1.0/9.0;
+        return new Matrix<double>(new double[,]{
+            { oneNinth, oneNinth, oneNinth },
+            { oneNinth, oneNinth, oneNinth },
+            { oneNinth, oneNinth, oneNinth },
+        });
+    }
+
+    /// <summary>
+    /// A weighted average blur that reduces image noise and detail, based on a Gaussian function
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> GaussianBlurKernel(int size) {
+        const double oneSixteenth = 1.0/16.0;
+        const double twoSixteenth = 2.0/16.0;
+        const double fourSixteenth = 4.0/16.0;
+        return new Matrix<double>(new double[,]{
+            { oneSixteenth, twoSixteenth, oneSixteenth },
+            { twoSixteenth, fourSixteenth, twoSixteenth },
+            { oneSixteenth, twoSixteenth, oneSixteenth },
+        });
+    }
+
+    /// <summary>
     /// Sobel kernel for edge detection, particularly for finding horizontal edges
     /// </summary>
     /// <returns>matrix</returns>
-    public static Matrix<double> SobelKernel() {
+    public static Matrix<double> SobelXKernel() {
         return new Matrix<double>(new double[,]{
             { -1, 0, 1 },
             { -2, 0, 2 },
@@ -68,10 +104,22 @@ public static class Kernels {
     }
 
     /// <summary>
+    /// Sobel kernel for edge detection, particularly for finding horizontal edges
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> SobelYKernel() {
+        return new Matrix<double>(new double[,]{
+            { -1, -2, -1 },
+            { 0, 0, 0 },
+            { 1, 2, 1 }
+        });
+    }
+
+    /// <summary>
     /// Prewitt kernel for edge detection, similar to Sobel but with different weights
     /// </summary>
     /// <returns>matrix</returns>
-    public static Matrix<double> PrewittKernel() {
+    public static Matrix<double> PrewittXKernel() {
         return new Matrix<double>(new double[,]{
             { -1, 0, 1 },
             { -1, 0, 1 },
@@ -80,14 +128,50 @@ public static class Kernels {
     }
 
     /// <summary>
-    /// This kernel leaves the image unchanged, effectively performing no operation
+    /// Prewitt kernel for edge detection, similar to Sobel but with different weights
     /// </summary>
     /// <returns>matrix</returns>
-    public static Matrix<double> IdentityKernel() {
+    public static Matrix<double> PrewittYKernel() {
         return new Matrix<double>(new double[,]{
+            { -1, -1, -1 },
             { 0, 0, 0 },
+            { 1, 1, 1 }
+        });
+    }
+
+    /// <summary>
+    /// Detects areas of rapid intensity change, commonly used for edge detection and sharpening
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> LaplacianKernel() {
+        return new Matrix<double>(new double[,]{
             { 0, 1, 0 },
-            { 0, 0, 0 }
+            { 1, -4, 1 },
+            { 0, 1, 0 }
+        });
+    }
+
+    /// <summary>
+    /// Enhances the edges and fine details of an image by emphasizing differences between neighboring pixels
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> SharpeningKernel() {
+        return new Matrix<double>(new double[,]{
+            { 0, -1, 0 },
+            {-1, 5, -1 },
+            { 0, -1, 0 }
+        });
+    }
+
+    /// <summary>
+    /// Gives the image a 3D shadow effect, emphasizing edges and contours
+    /// </summary>
+    /// <returns>matrix</returns>
+    public static Matrix<double> EmbossKernel() {
+        return new Matrix<double>(new double[,]{
+            { -2, -1, 0 },
+            {-1, 1, 1 },
+            { 0, 1, 2 }
         });
     }
 }
